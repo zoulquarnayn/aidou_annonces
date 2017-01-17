a61abb717201201abdc5756015d0e742dac1c1df
Faycal Ahmed <faycal.ahmed-alzouma-ext@pole-emploi.fr>
2017-01-17 23:37:11 +0100

    mise en place de la brique d'authentification FOSUserbundle : envoi d'un mail d'activation de compte après sa création.

--- src/AppBundle/Entity/Utilisateur.php
+++ src/AppBundle/Entity/Utilisateur.php
@@ -17,10 +17,36 @@ class Utilisateur extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+     /**
+     * @ORM\Column(type="string",nullable=true)
+     */
+    private $nom;
+    
+     /**
+     * @ORM\Column(type="string",nullable=true)
+     */
+    private $prenom;
 
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+    
+    public function getNom(){
+        return $this->nom;
+    }
+    
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+    
+    public function getPrenom(){
+        return $this->prenom;
+    }
+    
+    public function setPrenom($prenom){
+        $this->prenom=$prenom;
     }
 }
